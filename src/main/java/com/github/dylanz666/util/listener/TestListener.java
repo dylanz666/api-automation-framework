189e325135770b89f56292f7b54b9485344d2683
dylanz <zzh9098-=>
2020-09-03 23:00:35 +0800

    Enhance project

---
+++ src/main/java/com/github/dylanz666/util/listener/TestListener.java
@@ -0,0 +1,39 @@
+package com.github.dylanz666.util.listener;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/**
+ * @author : dylanz
+ * @since : 09/03/2020
+ */
+public class TestListener implements ITestListener {
+    public void onTestStart(ITestResult result) {
+        System.out.println("onTestStart...");
+    }
+
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("onTestSuccess...");
+    }
+
+    public void onTestFailure(ITestResult result) {
+        System.out.println("onTestFailure...");
+    }
+
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("onTestSkipped...");
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("onTestFailedButWithinSuccessPercentage...");
+    }
+
+    public void onStart(ITestContext context) {
+        System.out.println("onStart...");
+    }
+
+    public void onFinish(ITestContext context) {
+        System.out.println("onFinish...");
+    }
+}
